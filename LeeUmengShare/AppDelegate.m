056e36a60222372729be6a8096b9a528f1684ce4
yuanjilee <yuanjilee@icloud.com>
2015-06-08 22:49:57 +0800

    友盟分享Demo
    一个实现第三方分享—友盟分享的简单demo ,简单实用！

--- LeeUmengShare/AppDelegate.m
+++ LeeUmengShare/AppDelegate.m
@@ -6,7 +6,12 @@
 //  Copyright (c) 2015年 yuanjilee. All rights reserved.
 //
 
+
+
 #import "AppDelegate.h"
+#import "UMSocialQQHandler.h"
+#import "UMSocialWechatHandler.h"
+#import "UMSocialSinaSSOHandler.h"
 
 @interface AppDelegate ()
 
@@ -17,8 +22,36 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    [UMSocialData setAppKey:umeng_appkey];
+    //允许横屏
+    [UMSocialConfig setSupportedInterfaceOrientations:UIInterfaceOrientationMaskAll];
+    //未安装应用则隐藏分享图标，防止苹果审核受阻
+    [UMSocialConfig hiddenNotInstallPlatforms:@[UMShareToQQ,UMShareToQzone,UMShareToWechatSession,UMShareToWechatTimeline]];
+    //微信，QQ，新浪微博SSO
+    [UMSocialWechatHandler setWXAppId:weixin_appkey appSecret:weixin_appsecret url:@"http://www.baidu.com"];
+    [UMSocialQQHandler setQQWithAppId:qq_appID appKey:qq_appkey url:@"http://www.kk.net"];
+    [UMSocialSinaSSOHandler openNewSinaSSOWithRedirectURL:@"nil"];
+    
+
+    
     return YES;
 }
+
+
+//分享回调
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
+{
+    return  [UMSocialSnsService handleOpenURL:url];
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    return [UMSocialSnsService handleOpenURL:url];
+}
+
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- LeeUmengShare/ViewController.m
+++ LeeUmengShare/ViewController.m
@@ -6,7 +6,10 @@
 //  Copyright (c) 2015年 yuanjilee. All rights reserved.
 //
 
+
+
 #import "ViewController.h"
+
 
 @interface ViewController ()
 
@@ -17,6 +20,63 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake((ksize.width-100)/2.0, 200, 100, 100);
+    button.backgroundColor = [UIColor redColor];
+    [button addTarget:self action:@selector(buttonClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
+    [UMSocialData openLog:YES];
+}
+
+- (void)buttonClick
+{
+    //检查SSO授权是否过期
+    [UMSocialAccountManager isOauthAndTokenNotExpired:UMShareToSina];
+    NSArray *snsNames = [NSArray arrayWithObjects:UMShareToWechatTimeline,UMShareToWechatSession,UMShareToQzone,UMShareToQQ,UMShareToTencent,UMShareToSina,UMShareToSms, nil];
+    
+    //参数
+    
+    NSString *shareText = @"umengShareTest";
+    UIImage *shareImage = [UIImage imageNamed:@"Share.jpg"];
+    
+    //统一设置标题
+    [UMSocialData defaultData].extConfig.title = @"testTitle";
+    //分平台设置标题,朋友圈只显示标题
+    [UMSocialData defaultData].extConfig.wechatTimelineData.title = @"朋友圈Title";
+    
+    //url 当前仅发现必须分平台设置，appDelegate里已设置过，但此处优先级更高
+    [UMSocialData defaultData].extConfig.wechatTimelineData.url = @"www.baidu.com";
+    
+    
+    [UMSocialSnsService presentSnsIconSheetView:self appKey:umeng_appkey shareText:shareText shareImage:shareImage shareToSnsNames:snsNames delegate:self];
+    
+    
+}
+
+//判断分享平台，定制分享内容
+- (void)didSelectSocialPlatform:(NSString *)platformName withSocialData:(UMSocialData *)socialData
+{
+    if ([platformName isEqualToString:UMShareToSms]) {
+        socialData.shareImage = nil;
+    } else if ([platformName isEqualToString:UMShareToSina]) {
+        //微博分享没有标题，url跳转事件
+        socialData.shareText = @"新浪微博分享内容,url => www.baidu.com";
+    }
+    
+}
+
+//分享回调方法
+-(void)didFinishGetUMSocialDataInViewController:(UMSocialResponseEntity *)response
+{
+    //根据`responseCode`得到发送结果,如果分享成功
+    if(response.responseCode == UMSResponseCodeSuccess)
+    {
+        //得到分享到的微博平台名
+        NSLog(@"share to sns name is %@",[[response.data allKeys] objectAtIndex:0]);
+    }
 }
 
 - (void)didReceiveMemoryWarning {
